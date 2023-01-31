0e6943d0ee9d7d1ab02a0a9584bc7245918d2644
hampug1 <98275225+hampug1@users.noreply.github.com>
2023-01-31 16:40:22 +0000

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,14 @@
+
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+}
+
+
+
